db64edc6ccdf54a6f9438464601099f76f72bad9
devaminb <106449125+devaminb@users.noreply.github.com>
2022-08-18 00:42:24 +0200

    Rename currency filter to currency_filter
    The same name is used by Django Oscar which causes a conflict when using
    the two modules.

--- subscriptions/templatetags/currency_filters.py
+++ subscriptions/templatetags/currency_filters.py
@@ -7,7 +7,7 @@ from subscriptions.conf import SETTINGS
 register = template.Library()
 
 
-@register.filter(name='currency')
-def currency(value):
+@register.filter(name='currency_filter')
+def currency_filter(value):
     """Displays value as a currency based on the provided settings."""
     return SETTINGS['currency'].format_currency(value)
--- tests/subscriptions/test_currency_filters.py
+++ tests/subscriptions/test_currency_filters.py
@@ -1,10 +1,10 @@
 """Tests the currency_filters module."""
 from unittest.mock import patch
 
-from subscriptions.templatetags.currency_filters import currency
+from subscriptions.templatetags.currency_filters import currency_filter
 
 
 @patch.dict('subscriptions.conf.SETTINGS', currency_locale='en_us')
 def test_currency_filter():
     """Tests that value is properly returned as currency."""
-    assert currency('1000.005') == '$1,000.01'
+    assert currency_filter('1000.005') == '$1,000.01'
